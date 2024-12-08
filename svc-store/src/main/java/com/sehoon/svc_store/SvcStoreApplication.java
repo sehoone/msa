56b59dbf4169d1beff348ddfc054457a9c5f2da1
SeHoon <sehoon1989@gmail.com>
2024-12-08 18:18:27 +0900

    feature/add-eureka-client

--- svc-store/src/main/java/com/sehoon/svc_store/SvcStoreApplication.java
+++ svc-store/src/main/java/com/sehoon/svc_store/SvcStoreApplication.java
@@ -2,7 +2,9 @@ package com.sehoon.svc_store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient // 유레카 클라이언트로 등록
 @SpringBootApplication
 public class SvcStoreApplication {
 
